6b5aae6c315014ba8574d16a920b16ddb91e7caa
KongXiangX <760736325@qq.com>
2018-01-06 10:34:00 +0800

    Initial Commit

---
+++ MyFirstGame/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  MyFirstGame
+//
+//  Created by apple on 2018/1/6.
+//  Copyright © 2018年 GS. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ MyFirstGame/GameViewController.h
@@ -0,0 +1,15 @@
+//
+//  GameViewController.h
+//  MyFirstGame
+//
+//  Created by apple on 2018/1/6.
+//  Copyright © 2018年 GS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+#import <GameplayKit/GameplayKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
